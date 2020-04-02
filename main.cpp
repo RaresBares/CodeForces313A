89d6a0597cef29bfa449e62784e95ea2312316b4
Rares <>
2020-04-02 19:10:41 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+
+    string str;
+    cin >> str;
+
+    string last = str;
+    last.resize(last.length() - 1);
+    string plast = str.substr(str.length() - 1, str.length() - 1);
+
+
+    if (plast == "-0") {
+        plast = "0";
+    }
+
+    if (last == "-0") {
+        last = "0";
+    }
+
+
+
+    int ilast = stoi(last);
+    int iplast = stoi(plast);
+
+    if (ilast < stoi(str) && ilast < stoi(str)) {
+        cout << stoi(str);
+    } else {
+    if (ilast >= iplast) {
+
+        cout << ilast;
+    } else {
+        cout << iplast;
+    }
+}
+    return 0;
+}
